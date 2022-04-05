130ae20436c7d4816209a1c463c1e9dbfb8542d7
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-04-05 11:14:55 -0500

    setup is done

--- db/client.js
+++ db/client.js
@@ -1,2 +1,10 @@
 // build and export your unconnected client here
+// imports the pg modules 
+const { Client } = require('pg');
 
+const client = new Client('postgres://localhost:5432/fitness-dev');
+
+
+module.export = {
+    client
+}
