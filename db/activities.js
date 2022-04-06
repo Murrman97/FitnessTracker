6cd75025b9b5430c904edfe688976267ce1daba1
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-04-06 10:14:52 -0500

    update activity

--- db/activities.js
+++ db/activities.js
@@ -1,1 +1,58 @@
-const { client } = require('./client')
+const client = require("./client");
+
+async function createActivity({ name, description = [] }) {
+  try {
+    const {
+      rows: [activities],
+    } = await client.query(
+      `
+        INSERT INTO activities(name, description) 
+        VALUES ($1, $2)
+        RETURNING *;
+        `,
+      [name, description]
+    );
+    return activities;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllActivities() {
+  try {
+    const { rows: activitiesIds } = await client.query(`
+            SELECT id
+            FROM activities;
+          `);
+
+    return activitiesIds;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getActivityById(id) {
+  try {
+    const {
+      rows: [activities],
+    } = await client.query(
+      `
+        SELECT *
+        FROM activities
+        WHERE id=$1
+        `,
+      [activitiesIds]
+    );
+    return activities;
+  } catch (error) {
+    throw error;
+  }
+}
+async function updateActivity(id, name, description = {}) {}
+
+module.exports = {
+  createActivity,
+  getAllActivities,
+  getActivityById,
+  updateActivity,
+};
