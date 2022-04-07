f608c4f092931ef9f066a43c091b67af97edb1a0
Jacob Murray <Jacobjm74@icloud.com>
2022-04-07 12:15:20 -0400

    min changes

--- tests/db.spec.js
+++ tests/db.spec.js
@@ -284,7 +284,7 @@ describe("Database", () => {
         );
       });
     });
-    xdescribe("getPublicRoutinesByActivity", () => {
+    describe("getPublicRoutinesByActivity", () => {
       let routine, activity;
       beforeAll(async () => {
         activity = await getActivityById(3);
@@ -322,7 +322,7 @@ describe("Database", () => {
         );
       });
     });
-    xdescribe("createRoutine", () => {
+    describe("createRoutine", () => {
       it("creates and returns the new routine", async () => {
         routineToCreateAndUpdate = await createRoutine({
           creatorId: 2,
