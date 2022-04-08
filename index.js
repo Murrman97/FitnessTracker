68813e2b5c41ee11d688b3f397d1ea3749050b6b
Jacob Murray <Jacobjm74@icloud.com>
2022-04-08 12:55:55 -0400

    api start

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // create the express server here
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
